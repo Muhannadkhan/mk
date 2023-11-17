e28a0593a974e43293cea9d01f948c5463e9e591
Muhannadkhan <muhannadkhan66@gmail.com>
2023-11-17 12:25:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Components/Header';
+import './App.css';
+import Footer from './Components/Footer';
+import React from 'react';
+function App(){
+  return(
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
+
+  );
+}
+
+export default App;
